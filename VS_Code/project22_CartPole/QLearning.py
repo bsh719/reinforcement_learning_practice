7928a84294a01ac6f336f521a7364309beb54e8a
Siha Baek <123621949+bsh719@users.noreply.github.com>
2024-02-05 20:59:15 +0900

    Update QLearning.py

--- VS_Code/project22_CartPole/QLearning.py
+++ VS_Code/project22_CartPole/QLearning.py
@@ -8,7 +8,7 @@ import random
 
 # hyperparameters
 learning_rate = 0.01
-n_episodes = 1000
+n_episodes = 500
 print_interval = 10
 gamma = 0.99
 goal = 1000
@@ -22,10 +22,6 @@ class Q_net(nn.Module):
             nn.Linear(hidden, 2)
         )
         self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
-        self.episode = []
-
-    def put_data(self, x):
-        self.episode.append(x)
 
     def forward(self, x):
         out = self.model(x)
@@ -36,26 +32,8 @@ class Q_net(nn.Module):
         if t < epsilon:
             return random.randint(0, 1)
 
-        out = self(torch.tensor(state))
+        out = self(state)
         return out.argmax().item()
-
-    def train(self):
-        L = len(self.episode)
-        self.optimizer.zero_grad()
-
-        for t in range(L):
-            state, a, r, state2, done = self.episode[t]
-            out = self(torch.tensor(state))
-            target = r
-            if not done:
-                out2 = self(torch.tensor(state2))
-                target += torch.max(out2).item()
-            
-            loss = F.mse_loss(out[a], torch.tensor(target))
-            loss.backward()
-        
-        self.optimizer.step()
-        self.episode = []
 
 
 def main():
@@ -66,18 +44,29 @@ def main():
     for epi in range(n_episodes):
         epsilon = 1.0 / (10 + epi // 10)
         state = env.reset()[0]
+        state = torch.from_numpy(state)
         done = False
 
         for _ in range(goal):
             act = model.select_act(state, epsilon)
             state2, reward, done, _, _ = env.step(act)
+            state2 = torch.from_numpy(state2)
 
-            model.put_data((state, act, reward, state2, done))
+            # train
+            model.optimizer.zero_grad()
+            out = model(state)
+            target = torch.tensor(reward)
+            if not done:
+                out2 = model(state2)
+                target += gamma * torch.max(out2).item()
+
+            loss = F.mse_loss(out[act], target.detach())
+            loss.backward()
+            model.optimizer.step()
+            
             state = deepcopy(state2)
             score += reward
             if done: break
-
-        model.train()
 
         if epi % print_interval == print_interval - 1:
             print(f"episode: {epi+1}, average score: {score/print_interval}")
@@ -86,11 +75,13 @@ def main():
     # test
     env = gym.make("CartPole-v1", render_mode="human")
     state = env.reset()[0]
+    state = torch.from_numpy(state)
     done = False
     score = 0.0
-    for _ in range(goal * 2):
+    for _ in range(goal):
         act = model.select_act(state, epsilon)
         state, reward, done, _, _ = env.step(act)
+        state = torch.from_numpy(state)
         score += reward
         if done: break
 
