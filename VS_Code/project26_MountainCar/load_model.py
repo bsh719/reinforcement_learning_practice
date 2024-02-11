b58a9afa10e9106d049811fa4a64732366df841f
Siha Baek <123621949+bsh719@users.noreply.github.com>
2024-02-11 12:19:44 +0900

    Add files via upload

---
+++ VS_Code/project26_MountainCar/load_model.py
@@ -0,0 +1,22 @@
+import torch
+import gym
+from QLearning import QNet
+
+
+def main():
+    model = torch.load("project26_MountainCar/QLearning_model.pth")
+    env = gym.make("MountainCar-v0", render_mode="human")
+    s = env.reset()[0]
+    done = False
+    score = 0.0
+
+    while not done:
+        a = model.select_act(s, 0.0)
+        s, r, done, _, _ = env.step(a * 2)
+        score += r
+
+    print("score:", score)
+
+
+if __name__ == "__main__":
+    main()
